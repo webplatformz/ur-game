21aeba97fdf038583cb73fd2180e9877a2b90029
Lucas Schnüriger <lucas.schnueriger@me.com>
2022-08-27 18:24:14 +0200

    Pass dist directory for frontend as arg

--- deno/server.js
+++ deno/server.js
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std/http/mod.ts";
 import { serveFile } from "https://deno.land/std@0.153.0/http/file_server.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
 
 async function reqHandler(req) {
+	const appDistDir = parse(Deno.args).dist || '';
 	if (req.headers.get('upgrade') === 'websocket') {
 		const { socket: ws, response } = Deno.upgradeWebSocket(req);
 		ws.onmessage = e => ws.send(`Deno says: «${e.data}»`);
@@ -9,9 +11,9 @@ async function reqHandler(req) {
 	}
 	const url = new URL(req.url);
 	if (url.pathname.startsWith('/assets')) {
-		return serveFile(req, `${Deno.cwd()}/solid/dist/${url.pathname}`);
+		return serveFile(req, `${Deno.cwd()}/${appDistDir}/${url.pathname}`);
 	}
-	return serveFile(req, `${Deno.cwd()}/solid/dist/index.html`);
+	return serveFile(req, `${Deno.cwd()}/${appDistDir}/index.html`);
 }
 
 serve(reqHandler, { port: 42069 });
