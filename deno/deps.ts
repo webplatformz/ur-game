4abcb6e85886aab4800a41e07964cdb1153bb9ea
r3dDoX <r3dDoX@users.noreply.github.com>
2022-08-29 11:37:27 +0200

    use deps files as recommended by deno

---
+++ deno/deps.ts
@@ -0,0 +1,3 @@
+export { serve } from "https://deno.land/std@0.153.0/http/mod.ts";
+export { serveFile } from "https://deno.land/std@0.153.0/http/file_server.ts";
+export { parse } from "https://deno.land/std@0.153.0/flags/mod.ts";
