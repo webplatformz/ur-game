23440fa101529031291ee7a145c01da0fe19abaf
tispBe <timo.spring@zuehlke.com>
2022-08-29 16:20:14 +0200

    Add capacity board test

--- deno/tests/board.test.ts
+++ deno/tests/board.test.ts
@@ -1,12 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
 import { getBoardConfig } from "../game/board.ts";
 
-Deno.test("is field index same as board index", () => {
+Deno.test("should have field index same as board index", () => {
   const boardConfig = getBoardConfig();
   boardConfig.forEach((field, idx) => assertEquals(field.idx, idx));
 });
 
-Deno.test("can roll dices again on special fields", () => {
+Deno.test("should show roll dices again on special fields", () => {
   const boardConfig = getBoardConfig();
   const specialFieldIndeces = [4, 8, 14];
   boardConfig.forEach((field) => {
@@ -18,7 +18,7 @@ Deno.test("can roll dices again on special fields", () => {
   });
 });
 
-Deno.test("has safe zones", () => {
+Deno.test("should have safe zones", () => {
   const boardConfig = getBoardConfig();
   const safeZoneFields = [0, 1, 2, 3, 4, 8, 13, 14, 15];
   boardConfig.forEach((field) => {
@@ -26,7 +26,29 @@ Deno.test("has safe zones", () => {
         assertEquals(field.isSafe, true);
     }else{
         assertEquals(field.isSafe, false);
+    }
+  });
+});
+
+Deno.test("should have battlefield zones", () => {
+  const boardConfig = getBoardConfig();
+  const battlefieldFields = [5, 6, 7, 8, 9, 10, 11, 12];
+  boardConfig.forEach((field) => {
+    if(battlefieldFields.includes(field.idx)){
         assertEquals(field.isBattleField, true);
+    }else{
+        assertEquals(field.isBattleField, false);
+    }
+  });
+});
+
+Deno.test("should have correct field capacity", () => {
+  const boardConfig = getBoardConfig();
+  boardConfig.forEach((field) => {
+    if(field.idx === 0 || field.idx === 15){
+        assertEquals(field.capacity, 7);
+    }else{
+        assertEquals(field.capacity, 1);
     }
   });
 });
