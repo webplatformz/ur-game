ca4e3ac63505d18188a2499f0eae7a13ad619e1b
r3dDoX <r3dDoX@users.noreply.github.com>
2022-08-29 10:06:40 +0200

    create dist folder to exclude node modules

--- deno/server.js
+++ deno/server.js
@@ -3,7 +3,7 @@ import { serveFile } from "https://deno.land/std@0.153.0/http/file_server.ts";
 import { parse } from "https://deno.land/std/flags/mod.ts";
 
 async function reqHandler(req) {
-	const appDistDir = parse(Deno.args).dist || 'solid/dist';
+	const appDistDir = parse(Deno.args).dist || 'solid';
 	if (req.headers.get('upgrade') === 'websocket') {
 		const { socket: ws, response } = Deno.upgradeWebSocket(req);
 		ws.onmessage = e => ws.send(`Deno says: «${e.data}»`);
