6544f829746baae0c5a37f7d199f55a8fe878910
r3dDoX <r3dDoX@users.noreply.github.com>
2022-08-30 10:10:10 +0200

    cleanup sessions after disconnect

--- deno/websocket.ts
+++ deno/websocket.ts
@@ -1,31 +1,37 @@
-import { GameSession } from "./session/game-session.ts";
+import {GameSession} from './session/game-session.ts';
 
 let quickMatchWaitingPlayerSession: GameSession | undefined = undefined;
-const sessions: GameSession[] = [];
+let sessions: GameSession[] = [];
+
+function createCleanupSessionFunction(sessionId: string) {
+  return () => sessions = sessions.filter((session) => session.sessionId !== sessionId);
+}
 
 export function handleWebsocketConnection(
   socket: WebSocket,
   quickMatch: boolean,
   sessionId: string | null,
 ) {
-  if (quickMatch) {
+  if (sessionId) {
+    const foundSession = sessions.find((session) => session.sessionId === sessionId);
+    if (foundSession) {
+      foundSession.addPlayer(socket);
+    } else {
+      throw new Error(`No game session with id ${sessionId} found`);
+    }
+  } else if (quickMatch) {
     if (quickMatchWaitingPlayerSession) {
       quickMatchWaitingPlayerSession.addPlayer(socket);
       sessions.push(quickMatchWaitingPlayerSession);
+      quickMatchWaitingPlayerSession.onCleanUp = createCleanupSessionFunction(quickMatchWaitingPlayerSession.sessionId);
       quickMatchWaitingPlayerSession = undefined;
     } else {
       quickMatchWaitingPlayerSession = new GameSession(socket);
+      quickMatchWaitingPlayerSession.onCleanUp = () => quickMatchWaitingPlayerSession = undefined;
     }
   } else {
-    if (sessionId) {
-      const foundSession = sessions.find((session) => session.sessionId === sessionId);
-      if (foundSession) {
-        foundSession.addPlayer(socket);
-      } else {
-        throw new Error(`No game session with id ${sessionId} found`);
-      }
-    } else {
-      sessions.push(new GameSession(socket));
-    }
+    const gameSession = new GameSession(socket);
+    gameSession.onCleanUp = createCleanupSessionFunction(gameSession.sessionId);
+    sessions.push(gameSession);
   }
 }
