f8402f6293245c8a89112b1aa8e55012426bc3de
Lucas Schnüriger <lucas.schnueriger@me.com>
2022-08-30 12:08:05 +0200

    add move model for messaging

--- deno/shared/models/board-configuration.model.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore deno style imports
-import { Field } from "./field.model.ts";
-
-export interface BoardConfiguration {
-  fields: Field[];
-}
--- deno/shared/models/message-types.model.ts
+++ deno/shared/models/message-types.model.ts
@@ -4,6 +4,8 @@ import { GameState } from "./game-state.model.ts";
 import { Players } from "./players.model.ts";
 // @ts-ignore deno style imports
 import { DiceRoll } from "./dice-roll.model.ts";
+// @ts-ignore deno style imports
+import { Move } from "./move.model.ts";
 
 export type ClientMessageType = "ready" | "roll" | "move";
 export type ServerMessageType =
@@ -18,8 +20,7 @@ type WebsocketMessage<T extends MessageType, P> = { type: T } & P;
 export type WebsocketMessages =
   | WebsocketMessage<"roll", never>
   | WebsocketMessage<"gamestate", GameState>
-  // TODO add move message payload
-  | WebsocketMessage<"move", never>
+  | WebsocketMessage<"move", Move>
   | WebsocketMessage<"players", Players>
   | WebsocketMessage<"diceroll", DiceRoll>
   | WebsocketMessage<"ready", never>;
