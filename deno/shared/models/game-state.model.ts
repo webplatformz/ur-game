0b01ff58193cb317c89aac8018136c2a21cae4e3
r3dDoX <r3dDoX@users.noreply.github.com>
2022-08-30 14:44:00 +0200

    extract player color

--- deno/shared/models/game-state.model.ts
+++ deno/shared/models/game-state.model.ts
@@ -1,10 +1,12 @@
 // @ts-ignore deno style imports
 import { Field } from "./field.model.ts";
 
+export type PlayerColor = 'white' | 'black';
+
 export interface GameState {
   boardWhite: number[];
   boardBlack: number[];
-  currentPlayer: "white" | "black";
+  currentPlayer: PlayerColor;
   isFinished: boolean;
   boardConfig: Field[];
 }
