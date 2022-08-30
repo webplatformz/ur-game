7c67f5b8ff21f45c81a1fc4ae161647d2aa585f3
Tiziano Gemma <tiziano.gemma@zuehlke.com>
2022-08-30 11:54:51 +0200

    frontend: move Board and Mocks to board-screen.tsx

--- solid/src/screens/board-screen/board-screen.tsx
+++ solid/src/screens/board-screen/board-screen.tsx
@@ -1,14 +1,18 @@
-import { Component } from 'solid-js';
-import Board from './board';
-import StartingGamePieces from './starting-game-pieces';
-import Dice from './dice';
+import { Component } from "solid-js";
+import StartingGamePieces from "./starting-game-pieces";
+import Dice from "./dice";
 
-import styles from './board-screen.module.css'
+import styles from "./board-screen.module.css";
+import Board from "../../components/board/board";
+import { Mocks } from "../../Mocks";
 
 const BoardScreen: Component = () => {
   return (
     <div class={styles.BoardScreen}>
-      <Board class={styles.Board} />
+      <div class={styles.Board}>
+        <Mocks />
+        <Board />
+      </div>
       <StartingGamePieces class={styles.StartingPiecesPlayer1} />
       <StartingGamePieces class={styles.StartingPiecesPlayer2} />
       <Dice class={styles.Dice} />
