7df17ddc4c1e8fc12ec89c5ffde1d072d0327650
Schlumpf, Stefanie <stefanie.schlumpf@outlook.com>
2022-08-30 09:34:06 +0200

    do correct props handling in inputContainer

--- solid/src/components/login/login.tsx
+++ solid/src/components/login/login.tsx
@@ -2,31 +2,21 @@ import type {Component} from 'solid-js';
 
 import logo from '../../logo.svg';
 import styles from './login.module.css';
-import InputContaier from "../inputContainer/inputContainer";
+import InputContainer from "../inputContainer/inputContainer";
+import {createSignal} from "solid-js";
 
 const Login: Component = () => {
     const {host, protocol} = location;
     const socket = new WebSocket(`${protocol === 'https:' ? 'wss' : 'ws'}://${host}/ws`);
     socket.addEventListener('message', event => console.log('Message from server:', event.data));
 
-    // const [playerName, setPlayerName] = createSignal('');
-    // const sendPlayerName = () => socket.send(playerName());
-
+    // const sendPlayerName = (inputValue: string) => socket.send(inputValue);
+    const sendPlayerName = (inputValue: string) => console.log(inputValue);
 
     return (
         <div>
             <img class={styles.logo} src={logo} alt="logo"/>
-            <InputContaier/>
-
-            {/*<div class={styles.inputContainer}>
-                <input type="text" placeholder="Enter your name" maxLength={20}
-                       onInput={(e) => {
-                           setPlayerName(e.currentTarget.value);
-                       }}
-                />
-                <button id="btnSubmit" disabled={!playerName()} onClick={sendPlayerName}>Login</button>
-            </div>*/}
-
+            <InputContainer confirmInputValue={sendPlayerName} buttonText={"Login"} placeholderText={"Enter your name"}/>
         </div>
     );
 };
