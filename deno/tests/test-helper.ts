603b7dcd9435afb042a4b885544418ec6e6dbd76
Mario Kaufmann <mario.kaufmann@zuehlke.com>
2022-08-30 11:47:02 +0200

    Update tests

--- deno/tests/test-helper.ts
+++ deno/tests/test-helper.ts
@@ -3,7 +3,6 @@ import { getNewPlayerBoard } from "../game/player-board.ts";
 import { GameState } from "../shared/models/game-state.model.ts";
 
 const initialGameState: GameState = {
-  type: "gamestate",
   boardBlack: getNewPlayerBoard(),
   boardWhite: getNewPlayerBoard(),
   isFinished: false,
