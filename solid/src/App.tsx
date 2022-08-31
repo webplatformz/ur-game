c6b1eabf0f299e41d5a8b03046a172d3570a09dd
Julio Cesar Castro Lopez <caju@Julios-MacBook-Pro.local>
2022-08-31 14:38:29 +0200

    connect tokens with owner

--- solid/src/App.tsx
+++ solid/src/App.tsx
@@ -6,7 +6,8 @@ import styles from "./App.module.css";
 import Login from "./screens/login/login";
 import BoardScreen from "./screens/game/game";
 import { connectSocket } from "./connection/connection";
-import {navigationState, setNavigationState} from "./navigation";
+import { navigationState, setNavigationState } from "./navigation";
+import { Mocks } from "./Mock";
 
 const App: Component = () => {
   async function handleQuickStart() {
@@ -35,6 +36,7 @@ const App: Component = () => {
 
   return (
     <div class={styles.App}>
+      <Mocks />
       <Switch>
         <Match when={navigationState() === "LOGGED_OUT"}>
           <Header />
--- solid/src/components/field/field.tsx
+++ solid/src/components/field/field.tsx
@@ -1,10 +1,9 @@
-import {Component} from "solid-js";
-import {FieldOwner, useField} from "../../game/useField";
+import { Component, Show } from "solid-js";
+import { FieldOwner, useField } from "../../game/useField";
 import style from "./field.module.css";
-import rosette from "./../../assets/rosette.png"
-import empty from "./../../assets/empty.png"
+import rosette from "./../../assets/rosette.png";
+import empty from "./../../assets/empty.png";
 import Token from "../token/token";
-
 
 type FieldProps = {
   idx: number;
@@ -14,21 +13,26 @@ type FieldProps = {
 const Field: Component<FieldProps> = (
   { idx, owner },
 ) => {
-  const { tokenCount,config } = useField(idx, owner);
-  const {canThrowAgain} = config();
+  const { config, tokenCount, tokenOwner } = useField(idx, owner);
+
+  const { canThrowAgain } = config();
+  // const { count: oldTokenCount, owner: tokenOwner } = token();
 
   const chooseImage = () => {
-    if (canThrowAgain) {
-      return rosette;
-    }
+    if (canThrowAgain) return rosette;
+
     return empty;
   };
 
-
   return (
-    <div style={{"grid-area": `${owner}${idx}`}} class={style.field}>
-      <Token count={tokenCount()}/>
-      <img alt="square" class={style.fieldContent} src={chooseImage()}/>
+    <div
+      style={{ "grid-area": `${owner}${idx}` }}
+      class={style.field}
+    >
+      <Show when={tokenCount()}>
+        <Token count={tokenCount} owner={tokenOwner} />
+      </Show>
+      <img alt="square" class={style.fieldContent} src={chooseImage()} />
     </div>
   );
 };
