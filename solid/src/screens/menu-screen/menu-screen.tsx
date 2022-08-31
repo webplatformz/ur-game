445129c4ac74369605c6f1ca8e09cfd20e9ba06b
Tiziano Gemma <tiziano.gemma@zuehlke.com>
2022-08-31 16:45:52 +0200

    frontend: optimize manu layout

--- solid/src/screens/menu-screen/menu-screen.tsx
+++ solid/src/screens/menu-screen/menu-screen.tsx
@@ -1,25 +1,31 @@
-import { Component } from 'solid-js';
+import { Component } from "solid-js";
 
-import styles from './menu-screen.module.css';
-import Header from '../../components/header/header';
-import InputContainer from '../../components/inputContainer/inputContainer';
+import styles from "./menu-screen.module.css";
+import Header from "../../components/header/header";
+import InputContainer from "../../components/inputContainer/inputContainer";
 
 type Props = {
-  onHost: () => void
-  onJoin: (sessionId: string) => void
-  onQuickStart: () => void
-}
-const MenuScreen: Component<Props> = ({onHost, onJoin, onQuickStart}) => {
+  onHost: () => void;
+  onJoin: (sessionId: string) => void;
+  onQuickStart: () => void;
+};
+const MenuScreen: Component<Props> = ({ onHost, onJoin, onQuickStart }) => {
   return (
     <div class={styles.rootContainer}>
       <Header />
-      <button onClick={onQuickStart}>
-        Quick Start
-      </button>
-      <InputContainer confirmInputValue={onJoin} buttonText="Join" placeholderText="#0000" />
-      <button onClick={onHost} >
-        Host private game
-      </button>
+      <div class={styles.menuContainer}>
+        <button class={styles.menuEntry} onClick={onQuickStart}>
+          Quick Start
+        </button>
+        <InputContainer
+          confirmInputValue={onJoin}
+          buttonText="Join"
+          placeholderText="#0000"
+        />
+        <button class={styles.menuEntry} onClick={onHost}>
+          Host private game
+        </button>
+      </div>
     </div>
   );
 };
