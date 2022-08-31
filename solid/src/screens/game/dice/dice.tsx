2358914694dbc32db5143ba8c0c1a3ce521a61f6
Tiziano Gemma <tiziano.gemma@zuehlke.com>
2022-08-31 17:27:14 +0200

    frontend: display nr of tiles to move

--- solid/src/screens/game/dice/dice.tsx
+++ solid/src/screens/game/dice/dice.tsx
@@ -115,9 +115,22 @@ const Dice: Component<Props> = (props) => {
     return getDiceValues()[diceIndex] === 0;
   }
 
+  function getDiceResultAsNumber() {
+    return getDiceValues().reduce<number>((curr, prev) => prev + curr, 0);
+  }
+
   return (
     <div classList={{ [props.class]: true }}>
       <div classList={{ [styles.root]: true }}>
+        <div
+          classList={{
+            [styles.movesNrContainer]: true,
+            [styles.hidden]: !isRolledState(),
+          }}
+        >
+          Move <span class={styles.movesNr}>{getDiceResultAsNumber()}</span>{" "}
+          {getDiceResultAsNumber() === 1 ? "Tile" : "Tiles"}
+        </div>
         <svg
           class={styles.svgContainer}
           height="100%"
